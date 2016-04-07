4f378dc5c0767f4a0314aadd67f988590b6d957f
Frantisek Hartman <frantisek.hartman@seedcloud.com>
2016-04-07 10:13:29 +0100

    Example multimodule project

---
+++ submodule-a/src/test/java/com/example/testa/TestA.java
@@ -0,0 +1,9 @@
+package com.example.testa;
+
+public class TestA {
+
+    @org.junit.Test
+    public void testA() throws Exception {
+        System.out.println("Running TestA");
+    }
+}
---
+++ submodule-b/src/test/java/com/example/testb/TestB.java
@@ -0,0 +1,9 @@
+package com.example.testb;
+
+public class TestB {
+
+    @org.junit.Test
+    public void testB() throws Exception {
+        System.out.println("Running com.example.testb.TestB");
+    }
+}
